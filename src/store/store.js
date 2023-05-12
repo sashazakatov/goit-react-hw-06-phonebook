f8c4516fae631ff0acfc26078fd8d311a999ba96
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-05-12 21:13:07 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,40 +1,11 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
-import { nanoid } from 'nanoid'
 
-const contactsReducer = createSlice({
-    name: 'contacts',
-    initialState: JSON.parse(localStorage.getItem('contacts') ?? []),
-    reducer:{
-        addItem:{
-            reducer(state, actions){
-                state.push(actions.payload);
-            },
-            prepare({name, number}){
-                return{
-                    payload:{
-                        id: nanoid(),
-                        name,
-                        number,
-                    },
-                };
-            },
-        },
-    },
-});
- 
-const filterReducer = createSlice({
-    name: 'filter',
-    initialState: '',
-    reducers: {
-        setFilter(state, actions){
-            return actions.payload;
-        }
-    }
-})
+import { contactsReducer } from 'store/contactsSlice';
+import { filterReducer } from 'store/filterSlice'
 
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer.reducer,
-        filter: filterReducer.reducer,
+        contacts: contactsReducer,
+        filter: filterReducer,
     },
 })
